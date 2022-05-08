f292bde2a454c03b90b8d0b59f6d8d68041b208a
Elsaied samaka <elsaied.samaka1997@gmail.com>
2022-05-08 18:36:12 +0200

    setup products pagenetion

--- server/routes/products.js
+++ server/routes/products.js
@@ -4,12 +4,18 @@ const Product = require("../models/product");
 const Category = require("../models/category");
 const moment = require("moment");
 
-router.get("/", async (req, res) => {
+router.get( "/", async ( req, res ) => {
+  // 6 docmunts per page
+  const perPage = 6;
+  let page = parseInt(req.query.page) || 1;
   try {
     const products = await Product.find()
-      .sort({ createdAt: -1 })
-      .populate("category");
-    res.json(products);
+      .sort( { createdAt: -1 } )
+      .skip( ( page - 1 ) * perPage )
+      .limit( perPage )
+      .populate( "category" );
+    const count = await Product.count();
+    res.json( { products , count } );
   } catch (error) {
     console.log(`error: ${error.message}`);
     res.status(500).json({ message: error.message });
